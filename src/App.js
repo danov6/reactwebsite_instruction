d5ae0d02dfaa6118883f16c17d80e997535d5b0b
danov6 <glvaldez@callutheran.edu>
2019-05-14 00:12:15 -0700

    remove scripts

--- src/App.js
+++ src/App.js
@@ -719,12 +719,6 @@ class App extends React.Component {
 			<span></span>
 		</div>
 	</div>
-
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
-	<script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
       </div>
     );
   }
